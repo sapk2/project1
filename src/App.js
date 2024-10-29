5cf8dbc7d42f484f7423f15b5bf958cc2835b6b2
sapk2 <sapkotap521@gmail.com>
2024-10-29 22:27:21 +0545

    update

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          welcome to paribesh sapkota arena image carousel
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://python.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          let start reactjs
         </a>
       </header>
     </div>
